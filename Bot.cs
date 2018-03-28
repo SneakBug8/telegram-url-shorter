ac09b1ea7da24c608840d29545def8d4221227f7
SneakBug8 <sneakbug8@gmail.com>
2018-03-28 18:01:21 +0300

    Initial commit

---
+++ Bot.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+
+namespace url_shortener
+{
+    public class Bot
+    {
+        public TelegramBotClient Client;
+        public Bot(string id)
+        {
+            Initialize(id);
+        }
+
+        async Task Initialize(string id)
+        {
+            Client = new TelegramBotClient(id);
+            var me = await Client.GetMeAsync();
+            Console.Title = me.Username;
+            System.Console.WriteLine($"Hello! My name is {me.FirstName}");
+
+            Client.OnMessage += OnMessage;
+
+            Client.StartReceiving();
+        }
+
+        void OnMessage(object sender, MessageEventArgs args)
+        {
+            if (args.Message.Text == "/start") {
+                Client.SendTextMessageAsync(args.Message.Chat.Id, "Hi. I'm your handy shortening bot." + 
+                "Just send me Url and I'll shorten it for you");
+                return;
+            }
+            
+            Console.WriteLine(string.Format("Recieved message '{0}'.", args.Message.Text));
+            var message = Client.SendTextMessageAsync(args.Message.Chat.Id, "Processing your link...").Result;
+            var url = JsonConvert.DeserializeObject<Response>(GetShortenedUrl(args.Message.Text)).url;
+            Client.DeleteMessageAsync(args.Message.Chat.Id, message.MessageId);
+            Client.SendTextMessageAsync(args.Message.Chat.Id, "Your link is: " + url);
+        }
+
+        class Response {
+            public string status;
+            public string url;
+        }
+
+        string GetShortenedUrl(string url)
+        {
+            string site = string.Format("https://bit.ws/shorten?url={0}", url);
+
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(site);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            using (StreamReader stream = new StreamReader(
+                 resp.GetResponseStream(), Encoding.UTF8))
+            {
+                return stream.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace url_shortener
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Bot");
+            var Bot = new Bot("534058345:AAEZk7gjrOApzMYgqbaysy-mDefigs_xNl4");
+
+            while (true) {
+                
+            }
+        }
+    }
+}
